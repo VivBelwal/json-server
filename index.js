cf7edc4382855be7927d4ad3ec395de85b65832f
VivBelwal <04vivek10belwal.com>
2023-01-04 16:53:41 +0530

    create json server

---
+++ index.js
@@ -0,0 +1,15 @@
+const jsonServer = require('json-server');
+require("dotenv").config();
+const server = jsonServer.create();
+const port = process.env.PORT || 3001;
+console.log(process.env.PORT);
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port,() =>{
+    console.log(`Server running on port ${port}`);
+})
+
